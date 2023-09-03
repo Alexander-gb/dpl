c3606b79016a9289d1805002f876cd8cd363014e
Alexander <frosti-sizov@rambler.ru>
2023-09-03 14:01:57 +0300

    d1

--- src/main/java/com/example/diplom/domain/Message.java
+++ src/main/java/com/example/diplom/domain/Message.java
@@ -1,5 +1,6 @@
 package com.example.diplom.domain;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
---
+++ src/main/java/com/example/diplom/repos/UserRepo.java
@@ -0,0 +1,7 @@
+package com.example.diplom.repos;
+import com.example.diplom.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
